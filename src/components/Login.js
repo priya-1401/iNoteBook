a4a8ee1662a2693f755188366aaaa2818dcb0d06
priya-1401 <108124861+priya-1401@users.noreply.github.com>
2025-03-23 13:57:57 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ const Login = (props) => {
     let navigate=useNavigate();
     const handleSubmit=async (e)=>{
         e.preventDefault();
-        const response=await fetch("http://localhost:5000/api/auth/login",{
+        const response=await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/login`,{
             method:'POST',
             headers:{
                 'content-Type':'application/json'
