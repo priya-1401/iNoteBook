c06146443264b8a8abb14ec65a7f55aa15433f89
priya-1401 <108124861+priya-1401@users.noreply.github.com>
2025-03-23 13:59:07 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = (props) => {
     const handleSubmit=async (e)=>{
         e.preventDefault();
         const {name,email,password} = credentials;
-        const response=await fetch("http://localhost:5000/api/auth/createuser",{
+        const response=await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/createuser`,{
             method:'POST',
             headers:{
                 'content-Type':'application/json'
